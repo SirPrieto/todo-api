3d147dfc187b77a53120c680eb03abaca5b6c23a
SirPrieto <hi@abrahamprieto.com>
2022-02-12 16:49:03 +0000

    Main

--- src/js/component/todolist.js
+++ src/js/component/todolist.js
@@ -1,10 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { faCheckSquare } from "@fortawesome/free-solid-svg-icons";
 
 export const TodoList = () => {
 	const [inputValue, setInputValue] = useState("");
 	const [taskList, setTaskList] = useState([]);
+	const url = "https://assets.breatheco.de/apis/fake/todos/user/sirprieto";
+	useEffect(() => getFetch(), []);
+
+	// START getFetch Function
+
+	const getFetch = () => {
+		fetch(url)
+			.then((response) => {
+				if (!response.ok) {
+					throw Error(response.statusText);
+				}
+				// Read the response as json.
+				return response.json();
+			})
+			.then((responseAsJson) => {
+				// Do stuff with the JSONified response
+				console.log(responseAsJson);
+			})
+			.catch((error) => {
+				console.log("Looks like there was a problem: \n", error);
+			});
+	};
+	// END getFetch Function
+
+	// START putFecth Function
+
+	// END putFecth Function
+
+	// START delete/update Fecth Function
+
+	const updateFetch = (data) => {
+		fetch(url, {
+			method: "PUT", // or 'POST'
+			body: JSON.stringify(data), // data can be a `string` or  an {object} which comes from somewhere further above in our application
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((res) => res.json())
+			.then((response) => {
+				console.log("Success:", response);
+				getFetch();
+			})
+			.catch((error) => console.error("Error:", error));
+	};
+	// END delete Fecth Function
 
 	const targetValue = (e) => {
 		const newValue = e.target.value;
@@ -12,9 +59,10 @@ export const TodoList = () => {
 	};
 
 	const createTask = (e) => {
-		if (e.key === "Enter") {
-			setTaskList([...taskList, inputValue]);
+		if (e.keyCode == 13) {
+			setTaskList([...taskList, { label: inputValue, done: false }]);
 			setInputValue("");
+			updateFetch([...taskList, { label: inputValue, done: false }]);
 		}
 	};
 
@@ -23,6 +71,20 @@ export const TodoList = () => {
 			(taskDelete, index) => deletePosition != index
 		);
 		setTaskList(deleteValue);
+		updateFetch(deleteValue);
+	};
+
+	const markDone = (index) => {
+		const newTodos = taskList.map((task, i) => {
+			if (i == index) {
+				task.done = !task.done;
+				return task;
+			} else {
+				return task;
+			}
+		});
+		setTaskList(newTodos);
+		updateFetch(newTodos);
 	};
 
 	return (
@@ -39,14 +101,25 @@ export const TodoList = () => {
 			<ul className="list-group">
 				{taskList.map((task, index) => {
 					return (
-						<li
-							className="list-group-item"
-							onClick={() => deleteTask(index)}
-							key={index}>
-							{task}
-							<FontAwesomeIcon
-								className="iconPosition"
-								icon={faTrashCan}></FontAwesomeIcon>
+						<li className="list-group-item" key={index}>
+							{task.label}
+							<span
+								onClick={() => {
+									deleteTask(index);
+								}}>
+								<FontAwesomeIcon
+									className="iconPosition"
+									icon={faTrashCan}></FontAwesomeIcon>
+							</span>
+							<span
+								className={task.done ? "done" : ""}
+								onClick={() => {
+									markDone(index);
+								}}>
+								<FontAwesomeIcon
+									className="iconPosition"
+									icon={faCheckSquare}></FontAwesomeIcon>
+							</span>
 						</li>
 					);
 				})}
